2fa7ce28250c44c38e8019a036b845a303594810
SkoSik <skosiko@mail.ru>
2022-03-21 19:40:44 +0800

    8-9 back

--- SDNF.java
+++ SDNF.java
@@ -1,16 +1,24 @@
 package discrete_structures;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class SDNF extends NF {
 
-    public SDNF(String[] masks,int _vars) {
+    public SDNF(String[] masks, int _vars) {
         super(masks, _vars);
     }
 
-    public SDNF(ArrayList<char[]> masks,int _vars) {
+    public SDNF(ArrayList<char[]> masks, int _vars) {
         super(masks, _vars);
+    }
+
+    public SDNF(BinNumber binNumber) {
+        super(new ArrayList<char[]>(), binNumber.vars);
+        Arrays.stream(binNumber.array).forEach(a -> {
+            if (a == 1) set.add(new BinNumber(a));
+        });
     }
 
     public String toString() {
--- SKNF.java
+++ SKNF.java
@@ -1,6 +1,8 @@
 package discrete_structures;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class SKNF extends NF {
@@ -11,6 +13,13 @@ public class SKNF extends NF {
 
     public SKNF(ArrayList<char[]> masks, int _vars) {
         super(masks, _vars);
+    }
+
+    public SKNF(BinNumber binNumber) {
+        super(new ArrayList<char[]>(), binNumber.vars);
+        Arrays.stream(binNumber.array).forEach(a -> {
+            if (a == 0) set.add(new BinNumber(a));
+        });
     }
 
     public String toString() {
